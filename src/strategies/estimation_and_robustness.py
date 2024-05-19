4205fbb6f066c93d46df87e8eda895ec582fb6d9
Elhamza15 <elhamzaoui1509@gmail.com>
2024-05-19 11:59:56 +0200

    Commit Graph + Start

--- src/strategies/estimation_and_robustness.py
+++ src/strategies/estimation_and_robustness.py
@@ -4,9 +4,15 @@ from scipy.stats import ttest_1samp
 class Estimation:
     
     @staticmethod
-    def is_slope_significantly_positive(slope, alpha):
-        # Test de Student t à une seule échantillon avec alternative='greater'
-        t_statistic, p_value = ttest_1samp(slope, 0, alternative='greater', nan_policy ="omit")
+    def is_slope_positive_or_negative(slope, alpha, pos_or_neg):
+        
+        if pos_or_neg == 'pos':
+            alternative = 'greater'
+        elif pos_or_neg == 'neg':
+            alternative = 'less'
+            
+        # Test de Student t à une seule échantillon 
+        t_statistic, p_value = ttest_1samp(slope, 0, alternative=alternative, nan_policy ="omit")
         
         # Vérification de la p-value par rapport au niveau de significativité
         return p_value < alpha
