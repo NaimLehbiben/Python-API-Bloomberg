04c9cd0d2936bade1c99dcd0bc9013bd1682b4b8
Elhamza15 <elhamzaoui1509@gmail.com>
2024-05-22 17:51:41 +0200

    CpamFF + Graphs + RebalncingFreq

--- src/strategies/estimation_and_robustness.py
+++ src/strategies/estimation_and_robustness.py
@@ -1,5 +1,10 @@
 
 from scipy.stats import ttest_1samp
+import os
+import pandas as pd
+import statsmodels.api as sm
+from statsmodels.stats.sandwich_covariance import cov_hac
+import numpy as np
 
 class Estimation:
     
@@ -16,3 +21,54 @@ class Estimation:
         
         # Vérification de la p-value par rapport au niveau de significativité
         return p_value < alpha
+    
+
+    @staticmethod
+    def Cpam_FF_regress_statics(asset_indices):
+        
+        file_path = os.path.join(os.path.dirname(__file__).replace("src\\strategies", "data"), "FF_Factors.csv")
+        factors_df = pd.read_csv(file_path, sep=';')/100
+
+        statistics = {}
+
+        for strat_name in asset_indices.keys():
+            statistics[strat_name] = Estimation.__perform_factor_regression(asset_indices[strat_name], factors_df)
+
+
+        return statistics
+    
+    @staticmethod
+    def __perform_factor_regression(asset_index, factors):
+
+        prices = asset_index.quotes_to_dataframe()
+        prices.index = pd.to_datetime(prices.index)
+        monthly_returns = prices.resample('M').last().pct_change().dropna()
+
+        factors.index = monthly_returns.index
+        # Initialiser un dictionnaire pour stocker les résultats
+        results = {'CPAM': {}, 'Fama-French': {}}
+
+        # Régression 1: Rendements mensuels sur le facteur marché
+        mkt_factor = factors['Mkt']
+        results['CPAM']['coefficients'], results['CPAM']['tvalues'], results['CPAM']['rsquared'],results['CPAM']['is_significant'] = Estimation.__regress(mkt_factor, monthly_returns)
+
+        # Régression 2: Rendements mensuels sur les trois facteurs (marché, SMB, HML)
+        three_factors = factors[['Mkt', 'SMB', 'HML']]
+        results['Fama-French']['coefficients'], results['Fama-French']['tvalues'], results['Fama-French']['rsquared'],results['Fama-French']['is_significant'] = Estimation.__regress(three_factors, monthly_returns)
+
+        return results
+
+
+    @staticmethod
+    def __regress(X, y):
+        X = sm.add_constant(X)
+        model = sm.OLS(y, X).fit()
+        nw_cov = cov_hac(model)
+        nw_tvalues = model.params / np.sqrt(np.diag(nw_cov))
+        significance_indic = [
+            '**' if np.abs(t) > 1.96 else '*' if np.abs(t) > 1.645 else '' for t in nw_tvalues
+        ]
+
+        return model.params, nw_tvalues, model.rsquared, significance_indic
+
+
--- src/utils/utilities.py
+++ src/utils/utilities.py
@@ -5,6 +5,7 @@ import pickle
 import os
 from datetime import datetime
 import pandas_market_calendars as mcal
+import utils.config as config 
 
 class Utilities:
     """
@@ -12,37 +13,98 @@ class Utilities:
     """
 
     @staticmethod
-    def create_rebalancing_calendar(start_date: datetime, end_date: datetime):
+    def create_rebalancing_calendar(start_date: datetime, end_date: datetime, frequency: str = config.REBALANCING_FREQUENCY, rebalance_at: str = config.REBALANCING_MOMENT):
         """
-        Create a rebalancing calendar with business days between the start and end date.
+        Create a rebalancing calendar with business days between the start and end date, based on specified frequency and rebalancing point.
 
         Parameters:
         - start_date (datetime): The start date of the calendar.
         - end_date (datetime): The end date of the calendar.
+        - frequency (str): The frequency of rebalancing ('monthly', 'quarterly', 'semiannually', 'annually').
+        - rebalance_at (str): When to rebalance ('start' or 'end').
 
         Returns:
-        list[datetime]: A list of rebalancing dates from start_date to end_date
+        list[datetime]: A list of rebalancing dates from start_date to end_date.
         Raises:
-        ValueError: If start_date is after end_date.
+        ValueError: If start_date is after end_date, frequency is invalid, or rebalance_at is invalid.
         """
+        if start_date > end_date:
+            raise ValueError("start_date must be before end_date.")
+        if frequency not in ['monthly', 'quarterly', 'semiannually', 'annually']:
+            raise ValueError("Invalid frequency. Choose from 'monthly', 'quarterly', 'semiannually', 'annually'.")
+        if rebalance_at not in ['start', 'end']:
+            raise ValueError("Invalid rebalance_at. Choose from 'start' or 'end'.")
+
         nyse = mcal.get_calendar('NYSE')
         valid_dates = nyse.valid_days(start_date=start_date, end_date=end_date)
-        rebalnce_dates = [date.to_pydatetime().date() for i, date in enumerate(valid_dates[:-1]) if valid_dates[i+1].month != date.month]
-        rebalnce_dates.append(valid_dates[-1].date())
+        rebalnce_dates = []
+        
+
+        if frequency == 'monthly':
+            for i, date in enumerate(valid_dates[:-1]):
+                if rebalance_at == 'end' and valid_dates[i+1].month != date.month:
+                    rebalnce_dates.append(date.to_pydatetime().date())
+                elif rebalance_at == 'start' and valid_dates[i].month != valid_dates[i-1].month:
+                    rebalnce_dates.append(date.to_pydatetime().date())
+        elif frequency == 'quarterly':
+            for i, date in enumerate(valid_dates[:-1]):
+                if rebalance_at == 'end' and (valid_dates[i+1].month - 1) // 3 != (date.month - 1) // 3:
+                    rebalnce_dates.append(date.to_pydatetime().date())
+                elif rebalance_at == 'start' and (valid_dates[i].month - 1) // 3 != (valid_dates[i-1].month - 1) // 3:
+                    rebalnce_dates.append(date.to_pydatetime().date())
+        elif frequency == 'semiannually':
+            for i, date in enumerate(valid_dates[:-1]):
+                if rebalance_at == 'end' and (valid_dates[i+1].month - 1) // 6 != (date.month - 1) // 6:
+                    rebalnce_dates.append(date.to_pydatetime().date())
+                elif rebalance_at == 'start' and (valid_dates[i].month - 1) // 6 != (valid_dates[i-1].month - 1) // 6:
+                    rebalnce_dates.append(date.to_pydatetime().date())
+        elif frequency == 'annually':
+            for i, date in enumerate(valid_dates[:-1]):
+                if rebalance_at == 'end' and valid_dates[i+1].year != date.year:
+                    rebalnce_dates.append(date.to_pydatetime().date())
+                elif rebalance_at == 'start' and valid_dates[i].year != valid_dates[i-1].year:
+                    rebalnce_dates.append(date.to_pydatetime().date())
+
+        if valid_dates.empty:
+            rebalnce_dates.append(valid_dates[-1].date())
+        
         return rebalnce_dates
 
     @staticmethod
-    def get_rebalancing_date(date, step):
-        rebalancing_date = date + pd.DateOffset(months=step)
-        rebalancing_date = rebalancing_date + pd.offsets.MonthEnd(0)
+    def get_rebalancing_date(date, sign, rebalance_at = config.REBALANCING_MOMENT, step = None):
+        """
+        Get the rebalancing date after a given step, adjusted for business days.
+
+        Parameters:
+        - date (datetime): The initial date.
+        - step (int): The number of months to step forward.
+        - rebalance_at (str): When to rebalance ('start' or 'end').
+
+        Returns:
+        datetime: The rebalancing date adjusted to the nearest business day.
+        """
+        if step is None :
+            steps_dict = {'monthly' : 1, 'quarterly' : 3, 'semiannually':6, 'annually' : 12}
+            step = steps_dict[config.REBALANCING_FREQUENCY]
+            
+        if rebalance_at == 'end':
+            rebalancing_date = date + pd.DateOffset(months=step*sign)
+            rebalancing_date = rebalancing_date + pd.offsets.MonthEnd(0)
+        else:
+            rebalancing_date = date + pd.DateOffset(months=step*sign)
+            rebalancing_date = rebalancing_date - pd.offsets.MonthBegin(0) + pd.DateOffset(days=1)
+
         rebalancing_date = rebalancing_date.date()
-        nyse = get_calendar('XNYS')
-        valid_days_index = nyse.valid_days(start_date=(rebalancing_date-pd.Timedelta(days=3)), end_date=rebalancing_date)
+        nyse = mcal.get_calendar('NYSE')
+        valid_days_index = nyse.valid_days(start_date=(rebalancing_date - pd.Timedelta(days=3)), end_date=rebalancing_date)
         valid_days_list = [date.to_pydatetime().date() for date in valid_days_index]
+
         if not valid_days_list:
             return rebalancing_date
+
         while rebalancing_date not in valid_days_list:
             rebalancing_date -= pd.Timedelta(days=1)
+
         return rebalancing_date
 
     @staticmethod
@@ -79,28 +141,27 @@ class Utilities:
         return mean_weighted_returns_by_date.sort_index()
 
     @staticmethod
-    def get_data_from_pickle(file_name: str):
-        file_path = os.path.join(os.path.dirname(__file__).replace("src\\utils", "data"), file_name + ".pkl")
+    def get_data_from_pickle(file_name: str, folder_subpath :str = None):
+        if folder_subpath is None:
+            file_path = os.path.join(os.path.dirname(__file__).replace("src\\utils", "data"), file_name + ".pkl")
+        else:
+            file_path = os.path.join(os.path.dirname(__file__).replace("src\\utils", "data\\"), folder_subpath + "\\",file_name + ".pkl")
         with open(file_path, 'rb') as f:
             data = pickle.load(f)
         return data
 
     @staticmethod
-    def save_data_to_pickle(data, file_name):
-        file_path = os.path.join(os.path.dirname(__file__).replace("src\\utils", "data"), file_name + ".pkl")
+    def save_data_to_pickle(data, file_name, folder_subpath :str = None):
+        if folder_subpath is None:
+            file_path = os.path.join(os.path.dirname(__file__).replace("src\\utils", "data"), file_name + ".pkl")
+        else:
+            file_path = os.path.join(os.path.dirname(__file__).replace("src\\utils", "data\\"), folder_subpath + "\\",file_name + ".pkl")
         with open(file_path, 'wb') as f:
             pickle.dump(data, f)
 
     @staticmethod
-    def load_trackers(names_list: list[str]):
-        trackers = {}
+    def load_asset_indices(names_list: list[str], folder_subpath :str):
+        asset_indices = {}
         for name in names_list:
-            trackers.update({name: Utilities.get_data_from_pickle(name)})
-        return trackers
-
-    @staticmethod
-    def load_data(file_name):
-        file_path = os.path.join(os.path.dirname(__file__).replace("src\\utils", "data"), file_name + ".pkl")
-        with open(file_path, 'rb') as f:
-            data = pickle.load(f)
-        return data
+            asset_indices.update({name: Utilities.get_data_from_pickle(name, folder_subpath)})
+        return asset_indices
